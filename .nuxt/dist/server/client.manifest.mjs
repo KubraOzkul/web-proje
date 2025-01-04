76a8a70f1c098cf4f313e8c196ec2e49a238391d
KubraOzkul <Kubraozkul0@gmail.com>
2025-01-04 17:30:31 +0300

    Initial commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/90551/Desktop/github/proje/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+     <div class="container">
+      <h1>Component Viewer</h1>
+      <div class="button-container">
+        <button v-for="n in 10" :key="n" @click="navigateToComponent(n)">
+          Go to Component {{ n }}
+        </button>
+      </div>
+    </div>
+   
+    <HeaderComponent></HeaderComponent>
+
+
+    
+   
+    <CourselComponent></CourselComponent>
+    
+    <CompComponents></CompComponents>
+
+
+    <FooterComponents></FooterComponents>
+</template>
+
+<style>
+.bosluk{
+    padding: 50px;
+}
+
+</style>
+  
+  <script setup>
+  import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  
+  const navigateToComponent = (componentId) => {
+    router.push(`/component/${componentId}`);
+  };
+
+  const goToComponentTwo = () => {
+  // ComponentTwo'ya yönlendir yeni ekledim
+  router.push({ path: '/component/2' });
+}
+
+  </script>
+  
+  <style scoped>
+  .container {
+    text-align: center;
+    margin: 20px;
+  }
+  .button-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+  }
+  button {
+    padding: 10px 20px;
+    cursor: pointer;
+  }
+  </style>
+  

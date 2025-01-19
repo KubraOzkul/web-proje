01e4669304384908ae56d9d5e30b2f0226c24eb2
KubraOzkul <Kubraozkul0@gmail.com>
2025-01-19 22:29:29 +0300

    açılacak sepetim sayfası

---
+++ pages/sepet/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import SepetComponent from '~/components/sepetComponent.vue';
+
+</script>
+
+<template>
+    <ComponentOne/>
+    <ComponentEight/>
+    <sepetComponent/>
+</template>
